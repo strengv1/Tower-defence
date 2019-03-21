878d9f2002eb0ac967530a6624d116ea2ba3a99d
ville <ville.strengell@aalto.fi>
2019-03-21 12:00:05 +0200

    Basic main menu base, no implementations yet

--- main.py
+++ main.py
@@ -1,17 +1,55 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import *
+from PyQt5 import QtGui, QtCore
 
 
-def window():
+class App(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.title = "Main menu"
+        self.left = 300
+        self.top = 200
+        self.width = 640
+        self.height = 480
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.show()
+
+        play = QPushButton("Play")
+        howto = QPushButton("How to play")
+        exit = QPushButton("Exit")
+        settings = QPushButton("Settings")
+        menulabel = QLabel("Main menu")
+        menulabel.setAlignment(QtCore.Qt.AlignHCenter)
+
+        ylareuna = QHBoxLayout()
+        ylareuna.addStretch()
+        ylareuna.addWidget(settings)
+
+        v_box = QVBoxLayout()
+
+        v_box.addLayout(ylareuna)
+        v_box.addWidget(menulabel)
+        v_box.addStretch()
+        v_box.addWidget(play)
+        v_box.addStretch()
+        v_box.addWidget(howto)
+        v_box.addStretch()
+        v_box.addWidget(exit)
+
+
+
+
+        self.setLayout(v_box)
+
+if __name__ == '__main__':
     app = QApplication(sys.argv)
-    w = QWidget()
-    w.setWindowTitle('Ikkuna')
-
-
-    w.show()
-    sys.exit(app.exec())
-
-window()
+    ex = App()
+    sys.exit(app.exec_())
 
 
 
